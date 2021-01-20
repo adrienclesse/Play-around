d8a04a42a7b50b1d1c9e00cfa7073e27bf52259e
adrienclesse <adrienclesse@hotmail.com>
2021-01-20 10:53:46 +0100

    FIrst game finished with guesses

---
+++ Rock-paper-scisser/view.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Casino royale - rock, paper, scissors</title>
+</head>
+<body>
+<form  method="post">
+<button class="rock" name="rock">ROCK</button>
+<button class="paper" name="paper">PAPER</button>
+<button class="scissors" name="scissors">SCISSORS</button>
+</form>
+</body>
+</html>
